2f77bc1cdd975e185ee29c25076743e5266820ce
Dick Tseng <562276119@qq.com>
2017-01-02 05:16:57 +0800

    Modify logger: add lineNumber & file path

--- AuthLogin/log/logger.js
+++ AuthLogin/log/logger.js
@@ -1,6 +1,7 @@
-'use strict'
+'no use strict'
 
 const fs = require('fs');
+const path = require('path');
 
 let Logger = {};
 
@@ -29,7 +30,32 @@ Logger.log = function(content) {
 };
 
 Logger.console = function(content) {
-    console.log(new Date().toLocaleString('zh-CN') + ': ' + content);
+    let info = getCallInfo(1);
+    console.log(info.date, info.filename, 'line:' + info.line, content);
 };
+
+let getCallInfo = function(level) {
+    var orig, err, stack, line, column, filename;
+    var res = {
+        date: new Date().toLocaleString('zh-CN')
+    };
+    level = level || 0;
+    orig = Error.prepareStackTrace;
+    Error.prepareStackTrace = function(_, stack) {
+        return stack;
+    };
+    err = new Error();
+    Error.captureStackTrace(err, arguments.callee);
+    stack = err.stack;
+    Error.prepareStackTrace = orig;
+
+    if(stack && stack[level]) {
+        res.line = stack[level].getLineNumber();
+        res.column = stack[level].getColumnNumber();
+        res.filename = path.resolve(stack[level].getFileName());
+    }
+
+    return res;
+}
 
 module.exports = Logger;
